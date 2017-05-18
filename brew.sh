17eedaca6908592d8b9c60346509e33e323b6fc7
Iuri Gagnidze <igagnidz@gmail.com>
2017-05-18 09:46:24 -0500

    Updated brew commands

--- brew.sh
+++ brew.sh
@@ -10,7 +10,7 @@ brew upgrade
 
 # Install GNU core utilities (those that come with macOS are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
+#brew install coreutils
 
 # Install some other useful utilities like `sponge`.
 brew install moreutils
@@ -21,15 +21,15 @@ brew install gnu-sed --with-default-names
 # Install Bash 4.
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
 # running `chsh`.
-brew install bash
-brew tap homebrew/versions
-brew install bash-completion2
+#brew install bash
+#brew tap homebrew/versions
+#brew install bash-completion2
 
 # Switch to using brew-installed bash as default shell
-if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
-  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
-  chsh -s /usr/local/bin/bash;
-fi;
+#if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
+#  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
+#  chsh -s /usr/local/bin/bash;
+#fi;
 
 # Install `wget` with IRI support.
 brew install wget --with-iri
@@ -54,20 +54,20 @@ brew install wget --with-iri
 #brew install woff2
 
 # Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
+#brew install aircrack-ng
 brew install bfg
 brew install binutils
 brew install binwalk
 brew install cifer
-brew install dex2jar
+#brew install dex2jar
 brew install dns2tcp
-brew install fcrackzip
+#brew install fcrackzip
 brew install foremost
-brew install hashpump
+#brew install hashpump
 #brew install hydra
-brew install john
-brew install knock
-brew install netpbm
+#brew install john
+#brew install knock
+#brew install netpbm
 brew install nmap
 brew install pngcheck
 brew install socat
@@ -75,8 +75,8 @@ brew install sqlmap
 brew install tcpflow
 brew install tcpreplay
 brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
+#brew install ucspi-tcp # `tcpserver` etc.
+#brew install xpdf
 brew install xz
 
 # Install other useful binaries.
@@ -88,9 +88,9 @@ brew install ack
 #brew install imagemagick --with-webp
 #brew install lua
 #brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
+#brew install p7zip
+#brew install pigz
+#brew install pv
 brew install rename
 #brew install rhino
 brew install speedtest_cli
@@ -99,7 +99,11 @@ brew install testssl
 brew install tree
 brew install vbindiff
 #brew install webkit2png
-brew install zopfli
+#brew install zopfli
+brew install zstd
+brew install mongodb
+brew install redis
+brew install postgresql
 
 # Remove outdated versions from the cellar.
 brew cleanup
